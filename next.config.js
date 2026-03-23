acf0c2e4e1af91e464b24dae089a919b021d005a
Ester Batllori I Vila <74660957+esterbvila@users.noreply.github.com>
2026-03-23 18:32:07 +0100

    perf: tell swc to use browserslist targets, dropping legacy polyfills

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  experimental: {
+    browsersListForSwc: true,
+  },
 
   images: {
     formats: ['image/avif', 'image/webp'],
