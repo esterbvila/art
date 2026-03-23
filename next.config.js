69a673f68e66f7ae214e0069e62f7b20270dd5e3
Ester Batllori I Vila <74660957+esterbvila@users.noreply.github.com>
2026-03-23 12:11:57 +0100

    perf: add 512 to deviceSizes for better artist photo sizing

--- next.config.js
+++ next.config.js
@@ -5,7 +5,7 @@ const nextConfig = {
   images: {
     formats: ['image/avif', 'image/webp'],
     minimumCacheTTL: 2592000, // 30 days
-    deviceSizes: [640, 750, 828, 1080, 1280, 1440, 1920, 2048],
+    deviceSizes: [512, 640, 750, 828, 1080, 1280, 1440, 1920, 2048],
     imageSizes: [16, 32, 48, 64, 96, 128, 256, 320, 384, 512],
     remotePatterns: [
       {
